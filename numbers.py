a9d81612677b86e1ccb929eb6f9b1b4debacbc22
Abdullah <abdullahbozca@gmail.com>
2022-12-28 16:17:58 +0300

    28/12/22-3

---
+++ numbers.py
@@ -0,0 +1,22 @@
+x = 6.54
+
+# print(type(x)) # veri tipinin ne olduğunu söyler
+
+# print(x//3) # sadece tam sayı kısmını alır
+
+# print(x**3) # üssü alma işlemi yapar
+
+# print(x%3) # sayını 3 e bölümünden kalanı verir
+
+# print(abs(x)) # mutlak değer işlemi yapar
+
+# print(round(x)) # yuvarlama işlemi yapar
+ 
+# print(round(x,1)) # virgülden sonra 1 basamağı yuvarlar
+
+# print(pow(3,2)) # 3 ün 2. kuvvetini alır
+
+# print(min(1,5,9,8,88)) # minimum değeri yazar
+
+# print(max(1,5,9,8,88)) #maximum değeri yazar
+
