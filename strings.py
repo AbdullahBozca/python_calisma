de8efe3a38f97bcb766fa920fcff39dab1e550c7
Abdullah <abdullahbozca@gmail.com>
2022-12-28 10:14:19 +0300

    28/12/22-2

---
+++ strings.py
@@ -0,0 +1,17 @@
+name="Abdullah Bozca"
+
+# print(len(name))
+
+# print(name[1:12]) # substring 
+
+# print(name[:12])  # 0 dan başlayıp 12 ye kadar olan karakterleri yazar
+
+# print(name.upper()) # tüm harfleri büyük harfe çevirir
+
+# print(name.lower())   # tüm harfleri küçük harfe çevirir
+
+# print(name.count('a'))  # parametre olarak verilen karakterden kaç tane olduğunu döndürür
+
+# print(name.find('a')) # parametre olarak verilen karakterin kaçıncı indexte olduğunu döndürür
+
+print(name.replace("Bozca","Eymen")) 
