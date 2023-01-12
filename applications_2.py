2876852e2d5ca4afccf37fbbb9278045cf2cba9d
Abdullah <abdullahbozca@gmail.com>
2023-01-12 14:44:44 +0300

    12-01-23

---
+++ applications_2.py
@@ -0,0 +1,14 @@
+
+dersler=["türkçe","matematik","fizik","kimya","ingilizce"]
+
+# dizinin 2. ve son elamanini büyük harfle yazdir
+print(dersler[1].upper()+"   "+dersler[-1].upper())
+
+#fizik ve kimya elemanlarinin ciktisini al
+print(dersler[2:4])
+
+# ilk iki elemani yazdiran
+print(dersler[:2])
+
+#son iki elemani yazdiran
+print(dersler[-2:])
